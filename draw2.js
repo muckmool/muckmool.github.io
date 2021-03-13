90350aca7322520feb654d9af7244ba5cef0604f
muckmool <78839042+muckmool@users.noreply.github.com>
2021-03-13 16:40:27 +0900

    Update draw2.js

--- draw2.js
+++ draw2.js
@@ -5,12 +5,12 @@ var filter = "win16|win32|win64|mac|macintel";
 if ( navigator.platform ) { 
 
       if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
-            document.write("터치");
+            //document.write("터치");
             document.write('<script src ="https://muckmool.github.io/draw_touch.js"><', '/script>');
             
       } 
       else { 
-               document.write("마우스");
+               //document.write("마우스");
                document.write('<script src ="https://muckmool.github.io/draw_mouse.js"><', '/script>');
                
             } 
