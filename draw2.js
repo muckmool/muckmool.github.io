31bf54243f2854a2ff91585efab449ed72c0e4a4
muckmool <u-sung@hanmail.net>
2021-03-13 16:30:51 +0900

    0313

---
+++ draw2.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+
+var filter = "win16|win32|win64|mac|macintel"; 
+
+if ( navigator.platform ) { 
+
+      if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) { 
+            document.write("터치");
+            document.write('<script src ="https://muckmool.github.io/old/draw_touch.js"><', '/script>');
+            
+      } 
+      else { 
+               document.write("마우스");
+               document.write('<script src ="https://muckmool.github.io/old/draw_mouse.js"><', '/script>');
+               
+            } 
+}
+
+}
+
+
